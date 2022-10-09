eb7fc1f8b3770ca88c06def0b4c0340a53f997d3
Максим <fedyush90@mail.ru>
2022-10-09 22:12:31 +0300

    сумма цифр в числе

---
+++ dz_4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+try
+{
+    Console.WriteLine("Введите целое число");
+    int a = Math.Abs(int.Parse(Console.ReadLine()));
+    int sum = 0;
+    while (a > 99)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    sum = sum + a % 10 + a / 10;
+    Console.WriteLine($"сумма цифр введенного числа равна {sum}");
+}
+catch
+{
+    Console.WriteLine();
+}
