4b1a2cac0b5e91848de50ffa9665e6ac36a67704
Максим <fedyush90@mail.ru>
2022-10-10 22:00:20 +0300

    массив из 8 элементов

--- dz_4_3/Program.cs
+++ dz_4_3/Program.cs
@@ -12,8 +12,8 @@ void PrintArr(string [] array)
         }
     }
 }
-string [] arr = new string [array.Length]; // число 8 длинна массива
-for (int i = 0; i < array.Length; i++)
+string [] arr = new string [8]; // число 8 длинна массива)
+for (int i = 0; i < 8; i++)
 {
     Console.WriteLine($"введите {i + 1}-й элемент массива");
     arr[i] = Console.ReadLine();
